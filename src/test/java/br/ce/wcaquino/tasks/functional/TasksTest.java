4f3f5fd73337e41902a0e0a14c66b8df433dece0
Caio Augusto <caop18augusto@outlook.com>
2024-07-06 19:07:39 -0300

    Adicionando grid e docker compose

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -1,46 +1,81 @@
 package br.ce.wcaquino.tasks.functional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TasksTest {
     
     @Test
-    public void cadastrarTarefaSucesso() throws InterruptedException {
+    public void cadastrarTarefaSucesso() throws InterruptedException, MalformedURLException {
         // Define o caminho do ChromeDriver
         System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
 
-        // Inicializa o WebDriver
-        WebDriver driver = new ChromeDriver();
+        // Inicializa o WebDriver com o RemoteWebDriver e a URL correta
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.116:4444/wd/hub"), cap);
 
-        // Navega para o Google
-        driver.navigate().to("http://localhost:8001/tasks");
+        // Navega para a aplicação de tarefas com a URL correta
+        driver.navigate().to("http://192.168.0.116:8001/tasks");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         
-        driver.findElement(By.xpath("//*[@id=\"addTodo\"]")).click();
+        driver.findElement(By.id("addTodo")).click();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         
-        driver.findElement(By.xpath("//*[@id=\"task\"]")).sendKeys("Teste");
+        driver.findElement(By.id("task")).sendKeys("Teste");
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         
-        driver.findElement(By.xpath("//*[@id=\"dueDate\"]")).sendKeys("23/12/2029");
+        driver.findElement(By.id("dueDate")).sendKeys("23/12/2029");
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         
-        driver.findElement(By.xpath("//*[@id=\"saveButton\"]")).click();
+        driver.findElement(By.id("saveButton")).click();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         
-       //sucesso
-       String	message= driver.findElement(By.xpath("//*[@id=\"message\"]")).getText();
-       Assert.assertEquals("Sucess!", message);
+        // Verifica a mensagem de sucesso
+        String message = driver.findElement(By.id("message")).getText();
+        Assert.assertEquals("Sucess!", message);
         
         // Fecha o navegador
         driver.quit();
+    }
+    
+    @Test
+    public void cadastrarTarefaSucesso2() throws InterruptedException, MalformedURLException {
+        // Define o caminho do ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+
+        // Inicializa o WebDriver com o RemoteWebDriver e a URL correta
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.116:4444/wd/hub"), cap);
+
+        // Navega para a aplicação de tarefas com a URL correta
+        driver.navigate().to("http://192.168.0.116:8001/tasks");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         
+        driver.findElement(By.id("addTodo")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         
+        driver.findElement(By.id("task")).sendKeys("Teste QA");
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        
+        driver.findElement(By.id("dueDate")).sendKeys("23/12/2029");
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        
+        driver.findElement(By.id("saveButton")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        
+        // Verifica a mensagem de sucesso
+        String message = driver.findElement(By.id("message")).getText();
+        Assert.assertEquals("Sucess!", message);
+        
+        // Fecha o navegador
+        driver.quit();
     }
 }
